4839352cb2823a0167315ab4f6dfe60c4460f5ce
Douglas Santos <ds4208151@gmail.com>
2022-10-14 19:45:20 -0300

    working at linux

--- script.js
+++ script.js
@@ -1,70 +1,71 @@
-const buttonLogin = document.querySelector("#button-login");
-buttonLogin.addEventListener('click', validLogin)
+const buttonLogin = document.querySelector('#button-login');
 const acceptTerm = document.querySelector('#agreement');
 const comment = document.querySelector('#textarea');
-let value = 500;
-const button = document.querySelector("#submit-btn");
-comment.addEventListener('keyup', numberOfLetters);
+const value = 500;
+const button = document.querySelector('#submit-btn');
 
-acceptTerm.addEventListener('click', agreement)
+function validLogin(e) {
+  e.preventDefault();
+  const email = document.querySelector('#email').value;
+  const password = document.querySelector('#password').value;
 
-button.addEventListener('click', responseForm)
-
-function validLogin(e){
-    e.preventDefault();
-    const email = document.querySelector("#email").value;
-    const password = document.querySelector("#password").value;
-
-    if(email === "tryber@teste.com" && password === "123456"){
-        alert('Olá, Tryber!')
-    }
-    else {
-      alert('Email ou senha inválidos.')
-    }
-
+  if (email === 'tryber@teste.com' && password === '123456') {
+    alert('Olá, Tryber!');
+  } else {
+    alert('Email ou senha inválidos.');
+  }
 }
+buttonLogin.addEventListener('click', validLogin);
 
 function agreement() {
-    if(acceptTerm.checked){
-        button.disabled = false;
-    }else {
-        button.disabled = true;
-    }
+  if (acceptTerm.checked) {
+    button.disabled = false;
+  } else {
+    button.disabled = true;
+  }
 }
+acceptTerm.addEventListener('click', agreement);
 
 function numberOfLetters() {
-    const counter = document.querySelector('#counter');
-    counter.innerText = value - textarea.value.length;
+  const counter = document.querySelector('#counter');
+  counter.innerText = value - comment.value.length;
+}
+comment.addEventListener('keyup', numberOfLetters);
+
+function getValue(array) {
+  const result = [];
+  for (let x = 0; x < array.length; x += 1) {
+    result.push(` ${array[x].value}`);
+  }
+  return result;
+}
+
+function showUser(user) {
+  const response = Array.from(document.querySelectorAll('.response'));
+  let i = 0;
+  Object.keys(user).forEach((key) => {
+    response[i].innerText = user[key];
+    i += 1;
+  });
 }
 
 function responseForm(e) {
   e.preventDefault();
-  let i = 0;
+
   document.querySelector('#evaluation-form').style.display = 'none';
-  let user = {
-    name: 'Nome: ' + document.querySelector('#input-name').value + ' ' + document.querySelector('#input-lastname').value,
-    email: 'Email: ' + document.querySelector('#input-email').value,
-    house: 'Casa: ' + document.querySelector('#house').value,
-    family: 'Família: ' + document.querySelector('input[name="family"]:checked').value,
-    materias:'Matérias:'  + getValue(Array.from(document.querySelectorAll('input[class="subject"]:checked'))),
-    rating: 'Avaliação: ' + document.querySelector('input[name="rate"]:checked').value,
-    comments: 'Observações: ' + document.querySelector('#textarea').value
+  const user = {
+    name: `Nome: ${document.querySelector('#input-name').value}
+        ${document.querySelector('#input-lastname').value}`,
+    email: `Email: ${document.querySelector('#input-email').value}`,
+    house: `Casa: ${document.querySelector('#house').value}`,
+    family: `Família: ${document.querySelector('input[name="family"]:checked').value}`,
+    materias: `Matérias:${getValue(Array.from(
+      document.querySelectorAll('input[class="subject"]:checked'),
+    ))}`,
+    rating: `Avaliação: ${document.querySelector('input[name="rate"]:checked').value}`,
+    comments: `Observações: ${document.querySelector('#textarea').value}`,
   };
-
-  document.querySelector('.form-data').style.display ="block"
-  const response = Array.from(document.querySelectorAll(".response"));
-
-  for (const data in user) {
-      response[i].innerText = user[data]
-      i++;
-  }
-  
+  document.querySelector('.form-data').style.display = 'block';
+  showUser(user);
 }
-
-function getValue(array) {
-    let result = []
-   for (const el of array) {
-    result.push(' ' + el.value );
-  }
-   return result
-}
+button.addEventListener('click', responseForm);
